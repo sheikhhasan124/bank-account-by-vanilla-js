f1afd215d4496e04096c3842f0e94d1872a92390
hasan-ullah <mdhasanullah124@gmail.com>
2022-02-22 11:25:45 +0600

    deposit and withdraw button added

--- js/banking-me-two.js
+++ js/banking-me-two.js
@@ -1,1 +1,11 @@
-console.log('working')
+
+
+// deposit button event handler
+document.querySelector("#deposit-button").addEventListener("click", function(){
+console.log('clicli')
+ 
+ })
+ // with draw button event handler
+ document.querySelector("#with-draw-button").addEventListener("click", function(){
+  console.log('clicli')
+ })
