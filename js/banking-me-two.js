e5f2aa9a0a8dbba00017bcaacf8fdfc4e96633c7
hasan-ullah <mdhasanullah124@gmail.com>
2022-02-22 11:52:33 +0600

    total balance added

--- js/banking-me-two.js
+++ js/banking-me-two.js
@@ -14,16 +14,42 @@ function getDepoDrawAmount(inputValueAmount,depoDrawField){
   let newTotalAmountValue = previousTotalAmountValue + inputValueAmount;
    totalAmount.innerHTML = newTotalAmountValue;
 }
+// total balance field
+function getcurrentBalance(){
+  let totalBalance = document.querySelector('#balance-total');
+let previousTotalBalanceText = totalBalance.innerHTML;
+let previousTotalBalenceValue = parseFloat(previousTotalBalanceText);
+return previousTotalBalenceValue;
+}
+
+function getTotalBalance(inputValueAmount,isAdd){
+let totalBalance = document.querySelector('#balance-total');
+let previousTotalBalenceValue = getcurrentBalance();
+if(isAdd == true){
+  let newTotalBalenceValue = previousTotalBalenceValue + inputValueAmount;
+ totalBalance.innerHTML = newTotalBalenceValue;
+}else{
+  let newTotalBalenceValue = previousTotalBalenceValue - inputValueAmount;
+ totalBalance.innerHTML = newTotalBalenceValue;
+}
+}
 
 // deposit button event handler
 document.querySelector("#deposit-button").addEventListener("click", function(){
   let inputValueAmount =  getInputValue("#deposit-input");
-  getDepoDrawAmount(inputValueAmount,"#deposit-total");
+  if(inputValueAmount>0){
+    getDepoDrawAmount(inputValueAmount,"#deposit-total");
+    getTotalBalance(inputValueAmount,true);
+  }
+  
  
  })
  // with draw button event handler
  document.querySelector("#with-draw-button").addEventListener("click", function(){
   let inputValueAmount =  getInputValue("#with-draw-input");
+ let currentBalance = getcurrentBalance();
+if(inputValueAmount>0 && inputValueAmount<currentBalance){
   getDepoDrawAmount(inputValueAmount,"#with-draw")
-  
+  getTotalBalance(inputValueAmount,false);
+}
  })
