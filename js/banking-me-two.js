297d586247ff80f51b12114f9000dba4981925c1
hasan-ullah <mdhasanullah124@gmail.com>
2022-02-22 11:30:36 +0600

    common input value fucntion added

--- js/banking-me-two.js
+++ js/banking-me-two.js
@@ -1,11 +1,19 @@
-
+function getInputValue(input){
+  let inputValue = document.querySelector(input);
+  let inputValueText = inputValue.value;
+  let inputValueAmount = parseFloat(inputValueText);
+  // clear input value 
+  inputValue.value = "";
+  return inputValueAmount;
+  }
 
 // deposit button event handler
 document.querySelector("#deposit-button").addEventListener("click", function(){
-console.log('clicli')
- 
+  getInputValue("#deposit-input");
+ console.log(getInputValue)
  })
  // with draw button event handler
  document.querySelector("#with-draw-button").addEventListener("click", function(){
-  console.log('clicli')
+  getInputValue("#with-draw-input");
+  console.log(getInputValue)
  })
