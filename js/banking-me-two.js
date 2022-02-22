c1d6acc78a5a1d2dfe8d894152586bf6acc7c67e
hasan-ullah <mdhasanullah124@gmail.com>
2022-02-22 11:49:05 +0600

    deposit withdraw field added

--- js/banking-me-two.js
+++ js/banking-me-two.js
@@ -6,14 +6,24 @@ function getInputValue(input){
   inputValue.value = "";
   return inputValueAmount;
   }
+  //depo-withdraw total amount field
+function getDepoDrawAmount(inputValueAmount,depoDrawField){
+  let totalAmount = document.querySelector(depoDrawField);
+  let previousTotalAmountText = totalAmount.innerHTML;
+  let previousTotalAmountValue = parseFloat(previousTotalAmountText);
+  let newTotalAmountValue = previousTotalAmountValue + inputValueAmount;
+   totalAmount.innerHTML = newTotalAmountValue;
+}
 
 // deposit button event handler
 document.querySelector("#deposit-button").addEventListener("click", function(){
-  getInputValue("#deposit-input");
- console.log(getInputValue)
+  let inputValueAmount =  getInputValue("#deposit-input");
+  getDepoDrawAmount(inputValueAmount,"#deposit-total");
+ 
  })
  // with draw button event handler
  document.querySelector("#with-draw-button").addEventListener("click", function(){
-  getInputValue("#with-draw-input");
-  console.log(getInputValue)
+  let inputValueAmount =  getInputValue("#with-draw-input");
+  getDepoDrawAmount(inputValueAmount,"#with-draw")
+  
  })
